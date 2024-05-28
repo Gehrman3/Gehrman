34f0854d9d762b9bc13a9055dd84e1254d6aad89
Gehrman3 <123456789xdf3@gmail.com>
2024-05-28 22:35:19 +0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ import qs from "qs";
 const app = express();
 const port = 3000;
 const message=`TTTTr`;
-const post=`error..........،`;
+const post=`GM Turki`;
 // اذا كنت تبي تسوي سبام حط true و اذا لا حط false 👇
 // الاولى للدردشة والثانيه للمجتمع
 const sendMessageBool=true;
